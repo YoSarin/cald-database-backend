c8177406f5bfe7c651e8816d2e1ecbed3e576caf
yosarin <martin@yosarin.net>
2016-08-20 10:45:19 +0200

    initial commit

---
+++ src/routes.php
@@ -0,0 +1,17 @@
+<?php
+// Routes
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+$app->get('/check', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/check' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
