c17ad3cedbe6a126939a1f2b11ed4f6738cb7712
yosarin <martin@yosarin.net>
2016-10-04 18:05:41 +0200

    adding cors headers to OPTIONS as well

--- src/middleware.php
+++ src/middleware.php
@@ -5,5 +5,7 @@
 
 $app->add(function ($request, $response, $next) {
     return $next($request, $response)
-        ->withHeader('Access-Control-Allow-Origin', '*');
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
