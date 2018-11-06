f0d39830ac33a267abcd8ed3e6c1c45b34ad51af
Honza Novak <kacerr.cz@gmail.com>
2018-11-06 10:57:31 +0100

    Update build_container script

--- build_container.sh
+++ build_container.sh
@@ -1,3 +1,10 @@
-docker build -t kacerr/cald_backend:`git rev-parse HEAD` .
-docker push kacerr/cald_backend:`git rev-parse HEAD`
-echo kacerr/cald_backend:`git rev-parse HEAD`
+#!/bin/bash
+set -x
+image_name=cald_backend
+
+docker build -t $image_name:`git rev-parse HEAD` . --no-cache --build-arg GIT_COMMIT_HASH=`git rev-parse HEAD`
+
+final_tag="registry.gitlab.zlutazimnice.cz/yellow-fever/$image_name:`git rev-parse HEAD`"
+docker tag $image_name:`git rev-parse HEAD` $final_tag
+docker push $final_tag
+echo "Container has been pushed as: ${final_tag}"
