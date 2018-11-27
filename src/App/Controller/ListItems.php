1e2f2b2978941ebe19b460870f9dba7b9d709f4d
Martin <martin@yosarin.net>
2018-11-27 01:48:46 +0100

    removing unnecessary preload which actually does nothing, just breaks some stuff

--- src/App/Controller/ListItems.php
+++ src/App/Controller/ListItems.php
@@ -38,10 +38,6 @@ class ListItems extends \App\Common
 
         $model = "\\App\\Model\\" . ucfirst(\App\Model::camelcaseNotation($type));
         $joins = [];
-        if ($extend) {
-            // to preload and cache all extended data
-            $model::extendedJoins($joins);
-        }
         $data = $model::load($filter, $limit, $offset, $joins);
         $data = array_map(function ($item) use ($extend) {
             if ($extend) {
