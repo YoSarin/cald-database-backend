9ef5909a345d29506bcf4290101f4d6bc06c10c2
Honza Novak <kacerr.cz@gmail.com>
2019-12-03 20:53:30 +0100

    docker build: change target image repo url

--- build_container.sh
+++ build_container.sh
@@ -4,7 +4,7 @@ image_name=cald_backend
 
 docker build -t $image_name:`git rev-parse HEAD` . --no-cache --build-arg GIT_COMMIT_HASH=`git rev-parse HEAD`
 
-final_tag="registry.gitlab.zlutazimnice.cz/yellow-fever/$image_name:`git rev-parse HEAD`"
+final_tag="registry.gitlab.hrajfrisbee.cz/yellow-fever/$image_name:`git rev-parse HEAD`"
 docker tag $image_name:`git rev-parse HEAD` $final_tag
 docker push $final_tag
 echo "Container has been pushed as: ${final_tag}"
