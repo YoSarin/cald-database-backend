4a06e3dcfba777eae5db0f4bbbe38c35e05ba8a5
Martin <martin@yosarin.net>
2020-01-04 20:26:47 +0100

    #39 adding jersey_number parameter to user creation and modification API, adding jersey_number parameter to player_at_roster creation API

--- data/migrations/deploy/00009_adding_numbers_to_players.sql
+++ data/migrations/deploy/00009_adding_numbers_to_players.sql
@@ -1,5 +1,5 @@
 ALTER TABLE player
-    ADD COLUMN jersey_number NUMBER;
+    ADD COLUMN jersey_number int(11);
     
 ALTER TABLE player_at_roster
-    ADD COLUMN jersey_number NUMBER;
+    ADD COLUMN jersey_number int(11);
